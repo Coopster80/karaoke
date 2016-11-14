b9f94874e028e2fc17ee92abc5e99995004269d3
Nolan Cooper <nolancooper97@gmail.com>
2016-11-14 16:46:02 -0600

    Set up SQL file

---
+++ karaoke.sql
@@ -0,0 +1,27 @@
+/* Drop tables if they already exist*/
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Contributor;
+DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Title;
+DROP TABLE IF EXISTS File;
+DROP TABLE IF EXISTS Contributes;
+DROP TABLE IF EXISTS FreeAdd;
+DROP TABLE IF EXISTS PaidAdd;
+
+/* Create Tables*/
+CREATE TABLE User (
+);
+CREATE TABLE Contributor (
+);
+CREATE TABLE Artist (
+);
+CREATE TABLE Title (
+);
+CREATE TABLE File (
+);
+CREATE TABLE Contributes (
+);
+CREATE TABLE FreeAdd (
+);
+CREATE TABLE PaidAdd (
+);
