382f95252f75081ca499a15d12667303790b9e24
Nolan Cooper <nolancooper97@gmail.com>
2016-11-14 20:23:42 -0600

    Set up tables

--- karaoke.sql
+++ karaoke.sql
@@ -1,27 +1,52 @@
 /* Drop tables if they already exist*/
+DROP TABLE IF EXISTS Contributes;
+DROP TABLE IF EXISTS FreeAdd;
+DROP TABLE IF EXISTS PaidAdd;
+DROP TABLE IF EXISTS File;
 DROP TABLE IF EXISTS User;
 DROP TABLE IF EXISTS Contributor;
 DROP TABLE IF EXISTS Artist;
 DROP TABLE IF EXISTS Title;
-DROP TABLE IF EXISTS File;
-DROP TABLE IF EXISTS Contributes;
-DROP TABLE IF EXISTS FreeAdd;
-DROP TABLE IF EXISTS PaidAdd;
 
 /* Create Tables*/
 CREATE TABLE User (
-);
+	userID INT AUTO_INCREMENT PRIMARY KEY,
+	name varchar(100),
+	ccNum int);
 CREATE TABLE Contributor (
-);
+	contribID INT AUTO_INCREMENT PRIMARY KEY,
+	name varchar(100));
 CREATE TABLE Artist (
-);
+	artistID INT AUTO_INCREMENT PRIMARY KEY,
+	name varchar(100));
 CREATE TABLE Title (
-);
+	titleID INT AUTO_INCREMENT PRIMARY KEY,
+	text varchar(100));
 CREATE TABLE File (
-);
+	fileID INT AUTO_INCREMENT PRIMARY KEY,
+	path varchar(100),
+	artistID INT,
+	titleID INT,
+	FOREIGN KEY (artistID) REFERENCES Artist(artistID),
+	FOREIGN KEY (titleID) REFERENCES Title(titleID));
 CREATE TABLE Contributes (
-);
+	fileID INT,
+	contribID INT,
+	type varchar(100),
+	PRIMARY KEY(fileID, contribID),
+	FOREIGN KEY (fileID) REFERENCES File(fileID),
+	FOREIGN KEY (contribID) REFERENCES Contributor(contribID));
 CREATE TABLE FreeAdd (
-);
+	freeAddID INT AUTO_INCREMENT PRIMARY KEY,
+	userID INT,
+	fileID INT,
+	time TIME,
+	FOREIGN KEY (userID) REFERENCES User(userID),
+	FOREIGN KEY (fileID) REFERENCES File(fileID));
 CREATE TABLE PaidAdd (
-);
+	paidAddID INT AUTO_INCREMENT PRIMARY KEY,
+	userID INT,
+	fileID INT,
+	time TIME,
+	FOREIGN KEY (userID) REFERENCES User(userID),
+	FOREIGN KEY (fileID) REFERENCES File(fileID));
