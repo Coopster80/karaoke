52e80f54d4ebb3c392395858cb3b601cef0941be
Nolan Cooper <nolancooper97@gmail.com>
2016-11-20 19:54:29 -0600

    Add contributors to results

--- results.php
+++ results.php
@@ -18,8 +18,8 @@ catch(PDOexception $e) { // handle that exception
 		<link rel="icon" href="https://raw.githubusercontent.com/google/material-design-icons/master/av/2x_web/ic_queue_music_black_48dp.png" type="image/png">
 	</head>
 	<body>
-		<div class="main-content">
 		<h1>Search Results</h1>
+		<div class="search-results">
 		<table>
 			<tr>
 				<th>Title</th>
@@ -27,12 +27,21 @@ catch(PDOexception $e) { // handle that exception
 				<th>Contributors</th>
 			</tr>
 			<?
-			$sql = "SELECT T.text AS title, A.name AS artist FROM Artist A, Title T, File F WHERE T.text LIKE ? AND F.titleID = T.titleID AND F.artistID = A.artistID;";
+			$sql = "SELECT T.text AS title, A.name AS artist, F.fileID FROM Artist A, Title T, File F WHERE T.text LIKE ? AND F.titleID = T.titleID AND F.artistID = A.artistID;";
 			$stmt = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
 			$stmt->execute(array("%{$_POST['searchbox']}%"));
-			while ($result = $stmt->fetch(PDO::FETCH_BOTH))
+			while ($result = $stmt->fetch(pdo::FETCH_BOTH))
 			{
-				echo "<tr><td>" . $result[title] . "</td><td>" . $result[artist] . "</td></tr>";
+				echo "<tr><td>" . $result[title] . "</td><td>" . $result[artist] . "</td>";
+				$sql = "SELECT C.name, CC.type FROM Contributes CC, File F, Contributor C WHERE CC.fileID = F.fileID AND CC.contribID = C.contribID AND F.fileID = ?;";
+				$stmt2 = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+				$stmt2->execute(array($result['fileID']));
+				echo "<td>";
+				while ($result2 = $stmt2->fetch(pdo::FETCH_BOTH))
+				{
+					echo $result2[name] . " ";
+				}
+				echo "</td>";
 			}
 			?>
 		</table>
