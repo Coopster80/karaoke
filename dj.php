f23c689c1577a4f3b298dbfa2568d14c98d27935
Devin Lindelof <devin@devinlindelof.com>
2016-11-22 22:22:11 -0600

    fix dj select statements

--- dj.php
+++ dj.php
@@ -25,17 +25,30 @@ catch(PDOexception $e) { // handle that exception
      </thead>
     <tbody>
 
-    <!-- paid add view -->
+    <!-- 
+     - paid add view
+     - These items always come before the free queue items
+     - These items should have a different color text or have 
+       some visual change to let the DJ know they are the paid ones
+    -->
+    
     <?
       $sql = "SELECT F.titleID as Title, F.artistID as Artist, A.userID as Name FROM File F, PaidAdd A WHERE F.fileID = A.fileID GROUP BY F.titleID;"
-      $result = $pdo->query($sql);
+      $prepare = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+      $result = $prepare->execute(array($result['IDK WHAT TO PUT HERE']));
     ?>
 
 
-    <!-- free add view -->
+    <!-- 
+     - free add view 
+     - Comes after the paid queue items
+     - Normal text color?
+    -->
+
     <?
       $sql = "SELECT F.titleID as Title, F.artistID as Artist, A.userID as Name FROM File F, FreeAdd A WHERE F.fileID = A.fileID GROUP BY F.titleID;"
-      $result = $pdo->query($sql);
+      $prepare = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+      $result = $prepare->execute(array($result['IDK WHAT TO PUT HERE']));
     ?>
 
    </tbody>
