ccc3a14cd37016c4170013f40a09a86f3b1d51c1
Devin Lindelof <devin@devinlindelof.com>
2016-11-27 22:31:05 -0600

    Fixed DJ view

--- dj.php
+++ dj.php
@@ -8,6 +8,27 @@ catch(PDOexception $e) { // handle that exception
         echo "Connection to database failed: " . $e->getMessage();
 }
 ?>
+
+<?
+
+if($_POST['paidSelect'])
+{
+
+  $sql = "UPDATE PaidAdd SET played=1 WHERE paidAddID =" . $_POST['paidSelect'] . ";";
+  $result = $pdo->exec($sql);
+
+}
+
+if($_POST['freeSelect'])
+{
+
+  $sql = "UPDATE FreeAdd SET played=1 WHERE freeAddID =" . $_POST['freeSelect'] . ";";
+  $result = $pdo->exec($sql);
+
+}
+
+?>
+
 <html>
  <head>
    <meta charset="UTF-8">
@@ -19,7 +40,7 @@ catch(PDOexception $e) { // handle that exception
  </head>
  <body>
    <form id="DJ" method="post" action="dj.php">
-   <h1>Result list</h1>
+   <h1>Paid add list</h1>
    <div class="card">
    <table class="result-table">
     <thead>
@@ -32,47 +53,50 @@ catch(PDOexception $e) { // handle that exception
      </thead>
     <tbody>
     <?
+    //Paid add
 
-     /* 
-     - paid add view
-     - These items always come before the free queue items
-     -  Ordered by amount paid
-     - These items should have a different color text or have 
-       some visual change to let the DJ know they are the paid ones
-     */
-
-      if ($_POST[selected])
-      {
-        if ($_POST[paid])
-        {
-          $sql = "SELECT F.titleID as Title, F.artistID as Artist, A.userID as Name FROM File F, PaidAdd A WHERE F.fileID = A.fileID GROUP BY F.titleID;";
-          $prepare = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-          $prepare->execute(array($result['paid']));
-          while ($result = $prepare->fetch(pdo::FETCH_BOTH))
+          $sql = "SELECT P.paidAddID, T.text AS title, A.name as artist, U.name as user FROM Title T, File F, Artist A, User U, PaidAdd P WHERE F.fileID = P.fileID AND T.titleID = F.titleID AND A.artistID = F.artistID AND U.userID = P.userID AND P.played=0 ORDER BY P.amount DESC;";
+          $result = $pdo->query($sql);
+          while ($re = $result->fetch(pdo::FETCH_BOTH))
           {
-            echo "<tr class='item'><td><input type='radio' name='selected' value='" . "</td><td>" . $result[title] . "</td><td>" . $result[artist] . "</td><td>" . $result[userID] . "</td";
+            echo "<tr class='item'><td><input type='radio' name='paidSelect' value='" . $re[paidAddID] . "' id='" . $re[paidAddID] ."'></td><td>" . $re[title] . "</td><td>" . $re[artist] . "</td><td>" . $re[user] . "</td></tr>";
           }
-        }
-
-     /* 
-     - free add view 
-     - Comes after the paid queue items
-     - Normal text color?
-     */
-
-       else
-       {
-        $sql = "SELECT F.titleID as Title, F.artistID as Artist, A.userID as Name FROM File F, FreeAdd A WHERE F.fileID = A.fileID GROUP BY F.titleID;";
-        $prepare = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-        $prepare->execute(array($result['free']));
-        while ($result = $prepare->fetch(pdo::FETCH_BOTH))
-        {
-           echo "<tr class='item'><td><input type='radio' name='selected' value='" . "</td><td>" . $result[title] . "</td><td>" . $result[artist] . "</td><td>" . $result[userID] . "</td";
-        }
-       }
-      }
+          
     ?>
-   </tbody>
+      </tbody>
+     </table>
+               <input type="submit" value="clear">
+    </div>
+   </form>
+
+   <form id="DJ2" method="post" action="dj.php">
+   <h1>Free add list</h1>
+   <div class="card">
+   <table class="result-table">
+    <thead>
+     <tr>
+        <th>Select</th>
+        <th>Title</th>
+        <th>Artist</th>
+        <th>Name</th>
+      </tr>
+     </thead>
+    <tbody>
+    <?
+    //Free add
+
+          $sql = "SELECT FA.freeAddID, T.text AS title, A.name as artist, U.name as user FROM Title T, File F, Artist A, User U, FreeAdd FA WHERE F.fileID = FA.fileID AND T.titleID = F.titleID AND A.artistID = F.artistID AND U.userID = FA.userID AND FA.played=0;";
+          $result = $pdo->query($sql);
+          while ($re = $result->fetch(pdo::FETCH_BOTH))
+          {
+            echo "<tr class='item'><td><input type='radio' name='freeSelect' value='" . $re[freeAddID] . "' id='" . $re[freeAddID] ."'></td><td>" . $re[title] . "</td><td>" . $re[artist] . "</td><td>" . $re[user] . "</td></tr>";
+          }
+         
+    ?>
+     </tbody>
+    </table>
+             <input type="submit" value="clear">
+   </div>
   </form>
  </body>
 </html>
